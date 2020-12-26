0221ed07308b8f5925dfb1a7f6c29ec95ed45c01
salik <footballer.salik@gmail.com>
2020-12-26 21:55:38 +0530

    added login backend

---
+++ backend/server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'mongoUrl' : 'mongodb+srv://salik:1234@serverdata.iar9c.mongodb.net/serverData?retryWrites=true&w=majority',
+    'secretKey' : '12345-67890-09876-54321'
+
+
+}
---
+++ backend/server/routes/users.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+var passport = require('passport');
+var authenticate = require('../authenticate');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/signup',(req,res) => {
+  User.register(new User({username : req.body.username}),
+  req.body.password , (err,user) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
+    }
+    else {
+      if(req.body.firstname)
+        user.firstname = req.body.firstname;
+      if(req.body.lastname)
+        user.lastname = req.body.lastname;
+      if(req.body.email)
+        user.email = req.body.email;
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({err: err});
+            return ;
+          }
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success: true, status: 'You are successfully registered!'});
+        });  
+    }
+  });
+});
+
+router.post('/login',passport.authenticate('local'),(req,res) => {
+  var token = authenticate.getToken({_id : req.user._id});
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
+});
+
+router.get('/logout', (req, res) => {
+  // if (req.session) {
+  //   req.session.destroy();
+  //   res.clearCookie('session-id');
+  //   res.redirect('/');
+  // }
+  // else {
+  //   var err = new Error('You are not logged in!');
+  //   err.status = 403;
+  //   next(err);
+  // }
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true,  status: 'You are successfully logged out!',message : "yaa pr frontend se token del hona hai"});
+});
+
+
+
+module.exports = router;
